91653e8b87086851f8ea7a7ffe556c3536ab51e6
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2018-07-26 18:23:12 +0300

    Screw that, CoordPair is a class

--- GrammarEngineApi/CoordPair.cs
+++ GrammarEngineApi/CoordPair.cs
@@ -1,6 +1,6 @@
 ﻿namespace GrammarEngineApi
 {
-    public struct CoordPair
+    public class CoordPair
     {
         public CoordPair(int coordId, int stateId, string coordCode, string stateCode)
         {
@@ -18,15 +18,41 @@
 
         public string StateCode { get; }
 
+        protected bool Equals(CoordPair other)
+        {
+            return CoordId == other.CoordId && StateId == other.StateId && string.Equals(CoordCode, other.CoordCode) && string.Equals(StateCode, other.StateCode);
+        }
+
         public override bool Equals(object obj)
         {
-            CoordPair y = (CoordPair)obj;
-            return CoordId == y.CoordId && StateId == y.StateId;
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((CoordPair)obj);
         }
 
         public override int GetHashCode()
         {
-            return CoordId ^ (StateId << 10);
+            unchecked
+            {
+                var hashCode = CoordId;
+                hashCode = (hashCode * 397) ^ StateId;
+                hashCode = (hashCode * 397) ^ (CoordCode != null ? CoordCode.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (StateCode != null ? StateCode.GetHashCode() : 0);
+                return hashCode;
+            }
         }
 
         public override string ToString()
